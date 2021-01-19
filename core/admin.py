970bca4fb6b4fe1499288bf96fe66d9f9f79f06c
Tracy Falba <tfalba@mac.com>
2021-01-19 16:43:24 -0500

    added favicon and inlines for admin site -- check on heroku for both

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import Movie, User
 
-admin.site.register(Movie)
-admin.site.register(User)
+class MovieInline(admin.TabularInline):
+    model = Movie
+    list_display = ('title', 'release_year', 'is_nominee')
+    fields = ['title', 'release_year', 'is_nominee']
 
+    extra = 0
+
+# class UserInline(admin.TabularInline):
+#     model = User
+#     extra = 0
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_year', 'is_nominee')
+    # inlines = [UserInline]
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
+    fields = ['username']
+    inlines = [MovieInline]
+
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(User, UserAdmin)
